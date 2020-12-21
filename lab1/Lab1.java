799a97ec885973308955fcfb850391a4c15509b8
Carson Lin <kxxocarson@gmail.com>
2020-12-20 16:42:09 -0800

    completed first part of lab1

---
+++ lab1/Lab1.java
@@ -0,0 +1,17 @@
+public class Lab1{
+	public static void main(String[] args){
+		System.out.println("Hello world");
+
+		int x = 0;
+		int sum =0;
+		while(x < 10){
+			//x++;
+			sum+=x;
+			x++;
+			System.out.print(sum+ " ");
+		}
+		System.out.print(sum);
+	}
+
+}
+
