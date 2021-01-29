69354396040f4d110ee7b20cdabf6cf90095b6aa
Carson Lin <kxxocarson@gmail.com>
2021-01-29 03:22:25 -0800

    removed rendering in playWithInputString

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -3,7 +3,6 @@ package byog.Core;
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import edu.princeton.cs.algs4.Draw;
-import edu.princeton.cs.introcs.StdDraw;
 
 public class Game {
     TERenderer ter = new TERenderer();
@@ -33,43 +32,14 @@ public class Game {
         // TODO: Fill out this method to run the game using the input passed in,
         // and return a 2D tile representation of the world that would have been
         // drawn if the same inputs had been given to playWithKeyboard().
-        //int sizeOfInput = input.length();
-        //ter = new TERenderer();
-        //ter.initialize(80,50);
-        /*String seedToUse = input.substring(1,sizeOfInput - 1);
-        //System.out.println(seedToUse);
-
-        long seed = Long.parseLong(seedToUse);
-        DrawRandomMap newMap = new DrawRandomMap(80,50,seed);*/
-        DrawRandomMap newMap = processInput(input);
-        TETile[][] finalWorldFrame = newMap.generateWorld();
-        ter.renderFrame(finalWorldFrame);
-        return finalWorldFrame;
-    }
-
-    private DrawRandomMap processInput(String input){
-        if(input == null){
-            System.out.println("The input is null!");
-        }
-
-        String firstChar = Character.toString(input.charAt(0));
-
-        firstChar = firstChar.toLowerCase();
         int sizeOfInput = input.length();
 
         String seedToUse = input.substring(1,sizeOfInput - 1);
-        long seed = Long.parseLong(seedToUse);
         System.out.println(seedToUse);
 
-        switch(firstChar){
-            case "n":
-                DrawRandomMap newMap = new DrawRandomMap(80,50,seed);
-                return newMap;
-            default:
-                DrawRandomMap newMap2 = new DrawRandomMap(80,50,seed);
-                return newMap2;
-
-        }
-
+        long seed = Long.parseLong(seedToUse);
+        DrawRandomMap newMap = new DrawRandomMap(80,50,seed);
+        TETile[][] finalWorldFrame = newMap.generateWorld();
+        return finalWorldFrame;
     }
 }
--- proj2/byog/Core/Main.java
+++ proj2/byog/Core/Main.java
@@ -18,7 +18,7 @@ public class Main {
             Game game = new Game();
             TETile[][] worldState = game.playWithInputString(args[0]);
             System.out.println(TETile.toString(worldState));
-            //ter.renderFrame(worldState);
+            ter.renderFrame(worldState);
         } else {
             Game game = new Game();
             game.playWithKeyboard();
